07db7f269c26e29000b6d010d9d2a4ef75fe3fc6
oukhanabil2 <oukhanabil2@gmail.com>
2026-05-13 19:43:09 +0100

    Add CORS proxy handler in proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,5 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  if (req.method === 'OPTIONS') return res.status(200).end();
+  res.status(200).json({ message: 'Proxy OK' });
+}
